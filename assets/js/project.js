322ef2e28f873d1167a1ce1d33d72e0614ae9c43
mini-nijgal <dhamininijgal@gmail.com>
2024-01-24 19:34:41 +0530

    Image

--- assets/js/project.js
+++ assets/js/project.js
@@ -56,6 +56,7 @@ const showCards = () => {
     output += `       
         <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+          
             <div class="header">
             </div>
             <div class="data">
