ca9d31563abb0c7b565225fadb0aa393f1f3a8b8
Punith P <lillvznn@Puniths-MacBook-Air.local>
2024-02-12 19:03:46 +0530

    enhancement:[*footer rework]

--- assets/js/sem_temp.js
+++ assets/js/sem_temp.js
@@ -24,6 +24,7 @@ let header = $(`
  <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Star Board</a></li>
  <li class="nav-item nav-item-hover"><a class="nav-link" href="event.html">Gallery</a></li>
  <li class="nav-item nav-item-hover"><a class="nav-link" href="https://www.acengage.com/" target="_blank">Website</a></li>
+ <li class="nav-item nav-item-hover"><a class="nav-link" href="getHelp.html">Get Help</a></li>
  <li class="nav-item">
  <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
  </li>
@@ -259,4 +260,3 @@ const fillData = () => {
 };
 
 fillData();
-
